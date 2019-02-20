135d30a0a57f973c5fdf4591a09e64d07181a740
Aksel <aksel.sedum@outlook.com>
2019-02-20 17:17:36 +0100

    Init project
    Frame set and first test setup

---
+++ Poker/Hand.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Poker
+{
+    public class Hand
+    {
+        private static Dictionary<char, int> ValueToNumber = new Dictionary<char, int>();
+        private static Dictionary<char, Suits> CharToSuit = new Dictionary<char, Suits>();
+
+        private List<Card> cards = new List<Card>();
+        private Ranks rank = Ranks.NotSet;
+
+
+        static Hand()
+        {
+            ValueToNumber.Add('2', 2);
+            ValueToNumber.Add('3', 3);
+            ValueToNumber.Add('4', 4);
+            ValueToNumber.Add('5', 5);
+            ValueToNumber.Add('6', 6);
+            ValueToNumber.Add('7', 7);
+            ValueToNumber.Add('8', 8);
+            ValueToNumber.Add('9', 9);
+            ValueToNumber.Add('T', 10);
+            ValueToNumber.Add('J', 11);
+            ValueToNumber.Add('Q', 12);
+            ValueToNumber.Add('K', 13);
+            ValueToNumber.Add('A', 14);
+
+            CharToSuit.Add('H', Suits.Hearts);
+            CharToSuit.Add('C', Suits.Clubs);
+            CharToSuit.Add('D', Suits.Diamonds);
+            CharToSuit.Add('S', Suits.Spades);
+        }
+
+        public Hand(string hand)
+        {
+            foreach(var val in hand.Split(' '))
+            {
+                var parts = val.ToCharArray();
+                cards.Add(new Card(ValueToNumber[parts[0]], CharToSuit[parts[1]]));
+            }
+
+            var temp = new Dictionary<int, int>();
+            var sameSuit = true;
+            var compareSuit = cards[0].Suit;
+            foreach(var c in cards)
+            {
+                if (!temp.ContainsKey(c.Value)) temp.Add(c.Value, 0);
+                temp[c.Value]++;
+                if (c.Suit != compareSuit) sameSuit = false;
+            }
+            var straight = true;
+            var keys = temp.Keys.ToList();
+            if (keys.Contains(2) && keys.Contains(14))
+            {
+                keys.Remove(14);
+                keys.Add(1);
+            }
+            keys.Sort();
+            var first = keys[0];
+            for(int ix = 1; ix < 5; ix++)
+            {
+                if (keys[ix] != first + 1)
+                {
+                    straight = false;
+                    break;
+                }
+                first = keys[ix];
+            }
+            var profile = temp.Values.ToList();
+            profile.Sort();
+            profile.Reverse();
+            var result = string.Join("", profile.Select(p => p.ToString()).ToArray());
+            
+            switch(result)
+            {
+                case "11111":
+                    rank = Ranks.HighCard;
+                    if (sameSuit) rank = Ranks.Flush;
+                    if (straight && rank != Ranks.Flush) rank = Ranks.Straight;
+                    if (straight && sameSuit)
+                    {
+                        rank = keys[0] == 10 ? Ranks.RoyalFlush : Ranks.StraightFlush;
+                    }
+                    break;
+                case "2111":
+                    rank = Ranks.Pair;
+                    break;
+                case "221":
+                    rank = Ranks.TwoPairs;
+                    break;
+                case "311":
+                    rank = Ranks.ThreeOfAKind;
+                    break;
+                case "32":
+                    rank = Ranks.FullHouse;
+                    break;
+                case "41":
+                    rank = Ranks.FourOfAKind;
+                    break;
+                default:
+                    rank = Ranks.Error;
+                    break;
+            }
+        }
+
+        public int Total()
+        {
+            return cards.Select(c => c.Value).Sum();            
+        }
+
+        public Ranks RankValue() => rank;
+    }
+    public class Card
+    {
+        public int Value { get; set; }
+        public Suits Suit { get; set; }
+
+        public Card(int value, Suits suit)
+        {
+            Value = value;
+            Suit = suit;
+        }
+    }
+    public enum Suits { Hearts, Clubs, Diamonds, Spades }
+    public enum Ranks { Error = -1,
+                        NotSet = 0 ,
+                        HighCard = 1,
+                        Pair = 2,
+                        TwoPairs = 3,
+                        ThreeOfAKind = 4,
+                        Straight = 5,
+                        Flush = 6,
+                        FullHouse = 7,
+                        FourOfAKind = 8,
+                        StraightFlush = 9,
+                        RoyalFlush = 10 }
+}
---
+++ Poker/TestHands.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Poker
+{
+    public class TestHands
+    {
+        [Fact]
+        public void Test()
+        {
+            var mock = new Hand("KS 2D 3H 4C TD");
+            Xunit.Assert.Equal(13 + 2 + 3 + 4 + 10, mock.Total());
+            Xunit.Assert.Equal(Ranks.HighCard, mock.RankValue());
+        }
+
+        [Theory]
+        [InlineData("AC 4H 7D KC 2S", Ranks.HighCard)]
+        [InlineData("KC KH 7D 2C 5S", Ranks.Pair)]
+        [InlineData("KC KH 7D 7C 5S", Ranks.TwoPairs)]
+        [InlineData("KC KH KD 7C 5S", Ranks.ThreeOfAKind)]
+        [InlineData("3C 4H 5D 6C 7S", Ranks.Straight)]
+        [InlineData("2C 3H 4D 5C AS", Ranks.Straight)]
+        [InlineData("KC QC 9C 8C 2C", Ranks.Flush)]
+        [InlineData("KC KH KD 7C 7S", Ranks.FullHouse)]
+        [InlineData("KC KH KD KS 5S", Ranks.FourOfAKind)]
+        [InlineData("3C 4C 5C 6C 7C", Ranks.StraightFlush)]
+        [InlineData("TH JH QH KH AH", Ranks.RoyalFlush)]
+        public void TestRanks(string hand, Ranks rank)
+        {
+            var mock = new Hand(hand);
+            Assert.Equal(rank,mock.RankValue());
+        }
+    }
+}
